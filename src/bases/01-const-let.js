1e4f4f6b6f7fada5fdeb0f349b0c7c0dc91182c3
Airam A. <airam.a.g.85@gmail.com>
2023-10-19 20:56:36 +0100

    Variables y constantes

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// Variables y Constantes
+
+const name = 'John';
+const surname = 'Doe';
+
+let number = 1;
+
+console.log(name, surname, number);
+
+if(true) {
+    const name = 'Jane';
+    let number = 6;
+
+    console.log(name, number);
+}
+
+console.log(number);
