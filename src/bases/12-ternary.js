8af34d9bc0703cdf61eaf7ae5066f3780970d0a8
Airam A. <airam.a.g.85@gmail.com>
2023-10-23 20:01:06 +0100

    Operador condicional ternario

---
+++ src/bases/12-ternary.js
@@ -0,0 +1,16 @@
+// Operador ternario
+const enabled = true;
+
+// let msg = '';
+
+// if(enabled) {
+//     msg = 'enabled';
+// } else {
+//     msg = 'disabled';
+// }
+
+// const msg = (enabled) ? 'Enabled' : 'Disabled';
+// const msg = (!enabled) ? 'Enabled' : null;
+const msg = enabled && 'Enabled';
+
+console.log(msg);
